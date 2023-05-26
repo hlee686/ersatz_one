0b07e735c35b46303215d7bf6a29f80c42b777eb
Parallel <parallelkim12@gmail.com>
2023-05-26 14:19:40 +0900

    Add: Firebase key

--- src/api/fb/app.ts
+++ src/api/fb/app.ts
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyD4yqneoXkLm5BNs3nltEdAHN9lPosokQM",
+  authDomain: "react-tutorial-7378d.firebaseapp.com",
+  projectId: "react-tutorial-7378d",
+  storageBucket: "react-tutorial-7378d.appspot.com",
+  messagingSenderId: "1018732022697",
+  appId: "1:1018732022697:web:ee518b3af3a052154ed2b8",
+};
 
 export const app = initializeApp(firebaseConfig);
